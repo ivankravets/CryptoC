63cd0c9c192e01058d6d3f9c4ad34bdd71cea1d8
Snowda <sprkwear@gmail.com>
2013-11-07 16:43:16 -0800

    modified code syntax and defined basic functions to improve efficiency. Updated TODO list

--- TODO.md
+++ TODO.md
@@ -3,6 +3,13 @@ Todo list:
 ==============================
 
 Convert code into C from C++ to make the algorithm more compact for embedded systems.
+
 Support multiple data types.
+
 Make compatible across arduino versions and for more general usage outside the arduino platform if possible
+
 Replace SHA-256 with more secure algorithm as cryptography inevitably evolves in the future.
+
+Add Salting function for greater security.
+
+Find some way to increase efficeincy in calculation of hashes.
--- include/sha256.h
+++ include/sha256.h
@@ -7,40 +7,47 @@
 #define HASH_LENGTH 32
 #define BLOCK_LENGTH 64
 
+#define BUFFER_SIZE 64
+
+#define HMAC_IPAD 0x36
+#define HMAC_OPAD 0x5c
+
+#define ror32(number, bits) ((number << (32-bits)) | (number >> bits))
+
 union _buffer {
-  uint8_t b[BLOCK_LENGTH];
-  uint32_t w[BLOCK_LENGTH/4];
+    uint8_t b[BLOCK_LENGTH];
+    uint32_t w[BLOCK_LENGTH/4];
 };
 union _state {
-  uint8_t b[HASH_LENGTH];
-  uint32_t w[HASH_LENGTH/4];
+    uint8_t b[HASH_LENGTH];
+    uint32_t w[HASH_LENGTH/4];
 };
 
 class Sha256Class : public Print
 {
-  public:
-    void init(void);
-    void initHmac(const uint8_t* secret, int secretLength);
-    uint8_t* result(void);
-    uint8_t* resultHmac(void);
+    public:
+        void init(void);
+        void initHmac(const uint8_t* secret, int secretLength);
+        uint8_t* result(void);
+        uint8_t* resultHmac(void);
 #if defined(ARDUINO) && ARDUINO >= 100
-    virtual size_t write(uint8_t);
+        virtual size_t write(uint8_t);
 #else
-    virtual void write(uint8_t);
+        virtual void write(uint8_t);
 #endif
-    using Print::write;
-  private:
-    void pad();
-    void addUncounted(uint8_t data);
-    void hashBlock();
-    uint32_t ror32(uint32_t number, uint8_t bits);
-    _buffer buffer;
-    uint8_t bufferOffset;
-    _state state;
-    uint32_t byteCount;
-    uint8_t keyBuffer[BLOCK_LENGTH];
-    uint8_t innerHash[HASH_LENGTH];
+        using Print::write;
+    private:
+        void pad();
+        void addUncounted(uint8_t data);
+        void hashBlock();
+        _buffer buffer;
+        uint8_t bufferOffset;
+        _state state;
+        uint32_t byteCount;
+        uint8_t keyBuffer[BLOCK_LENGTH];
+        uint8_t innerHash[HASH_LENGTH];
 };
+
 extern Sha256Class Sha256;
 
 #endif
